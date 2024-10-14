2c8d24a93351a63ca782179a04ede4ab830b675d
alexitho <alexfloresortiz2226@gmail.com>
2024-10-14 10:12:12 -0500

    feat: clases dia lunes

--- main.py
+++ main.py
@@ -15,19 +15,76 @@
 # # deseo mostrar un menu con las opciones de agregar 
 # print(lista_alumnos)
 ## tarea
+# lista_alumnos=[]
+# def mensaje_menu():
+#     menu_opciones=input("""
+#     ------------Bienvenido al sistema------------
+#     -------------Registra tu alumno--------------
+    
+# 1. escribe  (s) si deseas registrar un alumno
+# 2. escribe  (n) si deseas salir del programa 
+# escribe la accion que deseas realizar: """)
+# print(menu_opciones)
+
+# while true:
+#     if menu_opciones. lower()== "n":
+#         print("saliendo del sistema")
+#         break
+#     if menu_opciones. lower()== "s":
+     
+# nombre=input("ingrese el nombre del alumno: ")
+# apellido=input("ingrese el apellido del alumno: ")
+# alumno=(
+#     "nombre":nombre,
+#     "apellido":apellido 
+# )
+# lista_alumnos.append(alumno)
+# print(lista_alumnos) 
+### clase_resolucion de la tarea con el profe_lunes_14
+
 lista_alumnos=[]
+def mensaje_menu():
+    menu_opciones = """
+    ------------Bienvenido al sistema------------
+    -------------Registra tu alumno--------------
+    
+1. escribe  (s) si deseas registrar un alumno
+2. escribe  (n) si deseas salir del programa 
+escribe la accion que deseas realizar: """
+    return menu_opciones
+
+def ingresar_datos_alumno():
+    id =len(lista_alumnos)+1
+    cui =int(input("ingrese el numero de dni: "))
+    nombre =input("ingrese el nombre del alumno: ")
+    apellido =input("ingrese el apellido del alumno: ")
+    numero_celular =int(input("ingrese el numero de celular: "))
+    programa_estudio =input("ingrese el programa de estudio")
+    ciclo_academico =input("ingrese su ciclo academico. ")
+    email =input("ingrese su correo electronico")
+
+def guardar_datos_alumno(id,cui,nombre,apellido,numero_celular,programa_estudio,ciclo_academico,email):
+         alumno={
+                "id":id,
+                "cui":cui,
+                "nombre":nombre,
+                "apellido":apellido,
+                "numero_celular":numero_celular,
+                "programa_estudio":programa_estudio,
+                "ciclo_academico":ciclo_academico,
+                "email":email
+               }
+        
+         lista_alumnos.append(alumno)
 while True:
-    opcion=input{"""elige lo que desas hacer
-                 escribe  (s) si deseas registrar un alumno
-                 escribe  (n) si deseas salir del programa 
-                 escribir tu resúesta: """}
-    if opcion. lower()=="n"
+   menu_opciones=input(mensaje_menu())
+
+   if menu_opciones.lower() == "n":
+       print("saliendo del sistema")
        break
-nombre=input("ingrese el nombre del alumno: ")
-apellido=input("ingrese el apellido del alumno: ")
-alumno=(
-    "nombre":nombre,
-    "apellido":apellido 
-)
-lista_alumnos.append(alumno)
-print(lista_alumnos) 
+   elif menu_opciones.lower() == "s":
+       ingresar_datos_alumno()
+   else:
+        print("opciones erronea")
+
+print(lista_alumnos)
